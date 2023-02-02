2c1e9d40435b0d644e81c976a0ee82bfc4a383d6
Mackenzie Roe <roe00021@algonquinlive.com>
2023-02-02 12:15:05 -0500

    adds initial week 4 content

---
+++ main.js
@@ -0,0 +1,62 @@
+//1. If...else conditional statements
+
+//create the variables 'userName' strgin; and 'userIsLoggedIn' boolean
+
+// Display the mesage 'Welcome, username' only if the user is logged in; output the 'message' using the HTML element with id 'loginMessage'
+
+
+//Switch conditional statements
+//create the variable 'loginResult' integer; 'icon' without value
+//create a switch to compare values from 0 to 3
+//asign, to the 'icon' variable, the string value of the name of an image... for every case. For example   case 0: icon = 'pass.png'
+//change the source attribute of the HTML element with the id 'loginResult' with the concatenation of the string 'img/' and the avriable 'icon'
+
+
+//Practice
+//  Toggle
+
+//  Display blogs
+//      if statement 
+//      Switch statement
+
+
+
+//2. Functions
+
+//String functions
+// console.log(userName.length)
+// console.log(userName.substring(0,3))
+// console.log(userName.replace('Plata', 'Platara'))
+// console.log(userName.toUpperCase())
+
+
+//Write a function that prints in the console 'firstName' 'lastName' and 'age'
+//Execute the function
+
+
+
+
+//Write a function that prints a new blog in the document
+// create more blogs
+// identify the variable that contains the HTML element where you want to print the new blog
+// retrieve the variable that contains the id 'addBlogBtn', addEventListener
+// declare the function
+// call the function
+
+
+// const $addBlogBtn = document.getElementById("addBlogBtn")
+// $element.addEventListener('click', function () {
+   
+// })
+
+
+//Write the login function: concatenate the message and return it
+
+
+
+
+
+
+
+
+  
